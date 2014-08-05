6befeb45550d7bfa5adf001be2c616cffc7988a0
emAmazed <emilieying@yahoo.com>
2014-08-05 15:42:27 -0400

    add operator parse

--- SimpleCalculator2.java
+++ SimpleCalculator2.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 public class SimpleCalculator2
 {
@@ -7,6 +10,13 @@ public class SimpleCalculator2
 		Scanner c = new Scanner(System.in);
 		System.out.print("Please Enter your Formula (Example: 12 x 3 + 4 / 2) \n");
 		String s = c.nextLine();
-		System.out.println("The result is: " + s);
+		System.out.println("You entered : " + s);
+
+		int answer;
+		ScriptEngineManager mgr = new ScriptEngineManager();
+		ScriptEngine engine = mgr.getEngineByName("JavaScript");
+
+		answer = ((Number)engine.eval( s )).intValue();
+		System.out.println("The result is : " + answer);
 	}
 }
